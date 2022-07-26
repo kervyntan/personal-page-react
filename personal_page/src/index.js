0aa2299086c18f28a4e1b35e5d9c1a3bffb5bb9e
iamkervyntan <kervyntan@gmail.com>
2022-07-26 16:36:01 +0800

    added firebase config into index.js file

--- personal_page/src/index.js
+++ personal_page/src/index.js
@@ -1,10 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { initializeApp } from "firebase/app"
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+const firebaseConfig = {
+  apiKey: "AIzaSyA_26Um8CHxc_uqMVTG1mYLxDvtht-c2Xg",
+  authDomain: "personal-page-51742.firebaseapp.com",
+  projectId: "personal-page-51742",
+  storageBucket: "personal-page-51742.appspot.com",
+  messagingSenderId: "483745827843",
+  appId: "1:483745827843:web:a9976f861ef3c5e2811236"
+};
+
+initializeApp(firebaseConfig);
 
 root.render(
   <StrictMode>
