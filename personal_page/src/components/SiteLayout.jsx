3008bfebc75d1066cab9d2e6e0177c82fb20134c
iamkervyntan <kervyntan@gmail.com>
2022-08-10 22:03:17 +0800

    added loading animation to application

--- personal_page/src/components/SiteLayout.jsx
+++ personal_page/src/components/SiteLayout.jsx
@@ -5,8 +5,14 @@ import Footer from "../shared/Footer";
 import cloud from "../assets/cloud.png";
 
 const SiteLayout = () => {
+  // adding loading icon and overlay
   return (
     <div className="container">
+      <div className="loading">
+        <div className="loading-ring">
+          <div></div>
+        </div>
+      </div>
       <Navbar
         img={cloud}
         firstItem="About"
